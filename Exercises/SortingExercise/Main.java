db454f00113d69e85854c4784f716941e1b0873b
Patrick Sørensen <62727344+RealSoerensen@users.noreply.github.com>
2022-11-28 13:18:00 +0100

    Update Main.java
    Co-Authored-By: Filip Koed Nymann <87913095+FleshIsBack@users.noreply.github.com>

--- Exercises/SortingExercise/Main.java
+++ Exercises/SortingExercise/Main.java
@@ -1,12 +1,28 @@
 package Exercises.SortingExercise;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = { 20, 35, -15, 7, 55, 1, -22 };
+        int[] array = new int[50];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        System.out.println("Unsorted array:");
+        printArray(array);
+
         Quicksort quicksort = new Quicksort(array);
         quicksort.sort();
-        for (int i = 0; i < quicksort.getArray().length; i++) {
-            System.out.println(array[i]);
+
+        System.out.println("Sorted array:");
+        printArray(quicksort.getArray());
+    }
+
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
+        System.out.println();
     }
 }
